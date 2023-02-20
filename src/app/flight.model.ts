df079a3d25592fda8eb591ba2ab2a6d3c2ce3257
gracielacesareo <graciela.cesareoc@gmail.com>
2023-02-20 12:41:34 -0600

    Add service

---
+++ src/app/flight.model.ts
@@ -0,0 +1,8 @@
+export interface Flight {
+    orign: string;
+    destination: string;
+    flightNumber: number;
+    depart: Date;
+    arrive: Date;
+    nonstop: boolean;
+};
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlightsService } from '../flights.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent {
+  constructor( private flightsService: FlightsService) {}
 
+  ngOnInit(): void {
+
+  }
 }
